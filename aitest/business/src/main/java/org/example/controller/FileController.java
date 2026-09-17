d5af7bd7612bc9000fbd3c83c5a641bb75a125cf
HuoQi <13704878047@163.com>
2026-09-17 11:22:03 +0800

    feat: 整理代码20

--- aitest/business/src/main/java/org/example/controller/FileController.java
+++ aitest/business/src/main/java/org/example/controller/FileController.java
@@ -2,31 +2,30 @@ package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 /**
  * @author hq
  */
 @RestController
 public class FileController {
-    private final WebClient aiWebClient;
+    private final WebClient ragWebClient;
 
-    public FileController(@Qualifier("ragWebClient")WebClient aiWebClient) {
-        this.aiWebClient = aiWebClient;
+    public FileController(@Qualifier("ragWebClient") WebClient ragWebClient) {
+        this.ragWebClient = ragWebClient;
     }
 
     /**
      * 流式对话接口（透传给 ai 模块）
      */
     @PostMapping(value = "/load")
-    public Flux<String> chatFlux(@RequestParam String path) {
-        return aiWebClient.post()
-                .uri("/load?path={path}", path)
+    public String chatFlux() {
+        return ragWebClient.post()
+                .uri("/load")
                 .retrieve()
-                .bodyToFlux(String.class);
+                .bodyToMono(String.class)
+                .block();
     }
 
 }
